80db76977f77245a62c177dddf2cdc93619e5530
Johannes Schacht <johannes@sch-8.de>
2021-05-17 22:38:42 +0200

    Functionality implemented

--- src/application.ts
+++ src/application.ts
@@ -4,19 +4,70 @@ export class MyCalculator {
 	public initialize(document: Document) {
 		this.document = document;
 		this.getElement("calculate").addEventListener("click", () => {
-			this.getElement("result").innerHTML = "Hello World";
+			this.calculate();
+		});
+		this.getInputElement("field1").addEventListener("input", () => {
+			this.calculate();
+		});
+		this.getInputElement("field2").addEventListener("input", () => {
+			this.calculate();
+		});
+		this.getSelectElement("operation").addEventListener("input", () => {
+			this.calculate();
 		});
 		this.getElement("result").innerHTML = "Result!";
 	}
 
-	/* 	public static init() {
+	private calculate() {
+		const val1 = parseInt(
+			(this.getElement("field1") as HTMLInputElement).value,
+			10
+		);
+		const val2 = parseInt(
+			(this.getElement("field2") as HTMLInputElement).value,
+			10
+		);
+		const opElem = this.getSelectElement("operation") as HTMLSelectElement;
+		const op = opElem.options[opElem.selectedIndex].text;
 
-	} */
+		let res: number = 0;
+		switch (op) {
+			case "+":
+				res = val1 + val2;
+				break;
+			case "-":
+				res = val1 - val2;
+				break;
+			case "*":
+				res = val1 * val2;
+				break;
+			case "/":
+				res = val1 / val2;
+				break;
+		}
+		this.getElement("result").innerHTML = res.toString();
+	}
 
 	public getElement(id: string): HTMLElement {
 		const elem = document.getElementById(id);
 		if (elem === null) throw new Error(`Not found: ${id}`);
 		return elem;
+	}
+
+	public getInputElement(id: string): HTMLInputElement {
+		const elem = document.querySelector(`input[id="${id}"]`);
+		if (elem instanceof HTMLInputElement) {
+			return elem as HTMLInputElement;
+		}
+		throw new Error(`Not found: ${id}`);
+	}
+
+	public getSelectElement(id: string): HTMLSelectElement {
+		const elem = document.querySelector(`select[id="${id}"]`);
+		if (elem instanceof HTMLSelectElement) {
+			return elem as HTMLSelectElement;
+		}
+		throw new Error(`Not found: ${id}`);
 	}
 }
 
